42db2f4c3469a00f7b5e725399b1705a10fed55f
longshorts <andrew@andrewoid.co.uk>
2017-10-13 12:25:29 +0100

    Further cleanup of structure, access modifiers.

--- Assets/Scripts/ElectrifiedEffect.cs
+++ Assets/Scripts/ElectrifiedEffect.cs
@@ -13,8 +13,8 @@ public class ElectrifiedEffect : MonoBehaviour {
     /// </summary>
     private struct EPoint
     {
-        public GameObject pointObject;
-        public Vector2 velocity;
+        public GameObject PointObject;
+        public Vector2 Velocity;
     }
 
     /// <summary>
@@ -22,18 +22,18 @@ public class ElectrifiedEffect : MonoBehaviour {
     /// </summary>
     private struct ELink
     {
-        public GameObject linkObject;
-        public LineRenderer lr;
-        public Transform pointA;
-        public Transform pointB;
+        public GameObject LinkObject;
+        public LineRenderer LR;
+        public Transform PointA;
+        public Transform PointB;
     }
 
-    public int numOfPoints = 5;
-    public float maxSpeed = 0.5f;
-    public float pointAltitude = 0.5f;
+    private const float k_lineWidth = 0.02f;
 
-    [SerializeField]
-    private GameObject pointPrefab;
+    [SerializeField] private int numOfPoints = 5;
+    [SerializeField] private float maxSpeed = 0.5f;
+    [SerializeField] private float pointAltitude = 0.5f;
+    [SerializeField] private GameObject pointPrefab;
 
     private MeshFilter m_mf;
     private EPoint[] m_points;
@@ -50,11 +50,10 @@ public class ElectrifiedEffect : MonoBehaviour {
 
         //Create the effect's EPoints and give them a random velocity
         m_points = new EPoint[numOfPoints];
-        for(int i = 0; i < numOfPoints; i++)
-        {
-            m_points[i].pointObject = GameObject.Instantiate(pointPrefab, transform);
-            m_points[i].pointObject.transform.localPosition = new Vector3(Random.Range(m_min.x, m_max.x), pointAltitude, Random.Range(m_min.z, m_max.z));
-            m_points[i].velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * maxSpeed;
+        for(int i = 0; i < numOfPoints; i++){
+            m_points[i].PointObject = GameObject.Instantiate(pointPrefab, transform);
+            m_points[i].PointObject.transform.localPosition = new Vector3(Random.Range(m_min.x, m_max.x), pointAltitude, Random.Range(m_min.z, m_max.z));
+            m_points[i].Velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * maxSpeed;
         }
 
         //Create a link between each couple of points
@@ -62,19 +61,19 @@ public class ElectrifiedEffect : MonoBehaviour {
         for(int i = 0; i < numOfPoints; i++){
             for(int j = i+1; j < numOfPoints; j++){
                 ELink link = new ELink();
-                link.pointA = m_points[i].pointObject.transform;
-                link.pointB = m_points[j].pointObject.transform;
+                link.PointA = m_points[i].PointObject.transform;
+                link.PointB = m_points[j].PointObject.transform;
 
-                link.linkObject = new GameObject();
-                link.lr = link.linkObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
-                link.lr.material = new Material(Shader.Find("Particles/Additive"));
-                link.lr.widthMultiplier = 0.02f*m_scale;
-                link.lr.positionCount = 2;
-                link.lr.startColor = Color.blue;
-                link.lr.endColor = Color.white;
+                link.LinkObject = new GameObject();
+                link.LR = link.LinkObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
+                link.LR.material = new Material(Shader.Find("Particles/Additive"));
+                link.LR.widthMultiplier = k_lineWidth * m_scale;
+                link.LR.positionCount = 2;
+                link.LR.startColor = Color.blue;
+                link.LR.endColor = Color.white;
 
-                link.lr.SetPosition(0, link.pointA.position);
-                link.lr.SetPosition(1, link.pointB.position);
+                link.LR.SetPosition(0, link.PointA.position);
+                link.LR.SetPosition(1, link.PointB.position);
                 m_links.Add(link);
             }
         }
@@ -83,36 +82,33 @@ public class ElectrifiedEffect : MonoBehaviour {
 	void Update () {
 
         #region EPoint movement
-        for(int i = 0; i < m_points.Length; i++)
-        {
-            Transform pt = m_points[i].pointObject.transform;
-            pt.localPosition += new Vector3(m_points[i].velocity.x,0,m_points[i].velocity.y) * Time.deltaTime;
+        for(int i = 0; i < m_points.Length; i++){
+            Transform pt = m_points[i].PointObject.transform;
+            pt.localPosition += new Vector3(m_points[i].Velocity.x,0,m_points[i].Velocity.y) * Time.deltaTime;
             //Reflect velocity if out of bounds
-            if(pt.localPosition.x < m_min.x || pt.localPosition.x > m_max.x)
-            {
-                m_points[i].velocity = new Vector2(m_points[i].velocity.x * -1, m_points[i].velocity.y);
-                pt.localPosition += new Vector3(m_points[i].velocity.x,0,m_points[i].velocity.y) * Time.deltaTime;
+            if(pt.localPosition.x < m_min.x || pt.localPosition.x > m_max.x){
+                m_points[i].Velocity = new Vector2(m_points[i].Velocity.x * -1, m_points[i].Velocity.y);
+                pt.localPosition += new Vector3(m_points[i].Velocity.x,0,m_points[i].Velocity.y) * Time.deltaTime;
             }
-            if (pt.localPosition.z < m_min.z || pt.localPosition.z > m_max.z)
-            {
-                m_points[i].velocity = new Vector2(m_points[i].velocity.x, m_points[i].velocity.y * -1);
-                pt.localPosition += new Vector3(m_points[i].velocity.x,0,m_points[i].velocity.y) * Time.deltaTime;
+            if (pt.localPosition.z < m_min.z || pt.localPosition.z > m_max.z){
+                m_points[i].Velocity = new Vector2(m_points[i].Velocity.x, m_points[i].Velocity.y * -1);
+                pt.localPosition += new Vector3(m_points[i].Velocity.x,0,m_points[i].Velocity.y) * Time.deltaTime;
             }
         }
         #endregion
 
         #region ELink rendering
         for (int i = 0; i < m_links.Count; i++){
-            m_links[i].lr.SetPosition(0, m_links[i].pointA.position);
-            m_links[i].lr.SetPosition(1, m_links[i].pointB.position);
-            float distance = Vector3.Distance(m_links[i].pointA.position, m_links[i].pointB.position);
-            Color c1 = m_links[i].lr.startColor;
-            Color c2 = m_links[i].lr.endColor;
+            m_links[i].LR.SetPosition(0, m_links[i].PointA.position);
+            m_links[i].LR.SetPosition(1, m_links[i].PointB.position);
+            float distance = Vector3.Distance(m_links[i].PointA.position, m_links[i].PointB.position);
+            Color c1 = m_links[i].LR.startColor;
+            Color c2 = m_links[i].LR.endColor;
             //Alpha is calculated according to the distance between each point, relative to the plane's scale.
             c1.a = Mathf.Max(2.0f - distance/m_scale, 0.0f);
             c2.a = c1.a;
-            m_links[i].lr.startColor = c1;
-            m_links[i].lr.endColor = c2;
+            m_links[i].LR.startColor = c1;
+            m_links[i].LR.endColor = c2;
         }
         #endregion
     }
